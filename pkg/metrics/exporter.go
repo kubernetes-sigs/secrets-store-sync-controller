569742b2ec0ff963a19ccc94b93c209197c03970
Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>
2024-06-28 09:32:21 -0700

    feat: sets up code in new repo (#12)
    Signed-off-by: Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>

---
+++ pkg/metrics/exporter.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"k8s.io/klog/v2"
+)
+
+var (
+	metricsBackend = flag.String("metrics-backend", "Prometheus", "Backend used for metrics")
+)
+
+const prometheusExporter = "prometheus"
+
+func InitMetricsExporter() error {
+	mb := strings.ToLower(*metricsBackend)
+	klog.InfoS("initializing metrics backend", "backend", mb)
+	switch mb {
+	// Prometheus is the only supported exporter
+	case prometheusExporter:
+		return initPrometheusExporter()
+	default:
+		return fmt.Errorf("unsupported metrics backend %v", *metricsBackend)
+	}
+}
