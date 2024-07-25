2a7ce23e709fbfd2539a197490898a663febaafa
Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>
2024-07-25 09:56:26 -0700

    test: adds unit tests for Reconcile (#36)
    Signed-off-by: Nilekh Chaudhari <1626598+nilekhc@users.noreply.github.com>

--- internal/controller/secretsync_controller_test.go
+++ internal/controller/secretsync_controller_test.go
@@ -18,12 +18,13 @@ package controller
 
 import (
 	"context"
-	"errors"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -35,10 +36,22 @@ import (
 	secretsstorecsiv1 "sigs.k8s.io/secrets-store-csi-driver/apis/v1"
 	providerfake "sigs.k8s.io/secrets-store-csi-driver/provider/fake"
 	"sigs.k8s.io/secrets-store-csi-driver/provider/v1alpha1"
+
 	secretsyncv1alpha1 "sigs.k8s.io/secrets-store-sync-controller/api/v1alpha1"
 	"sigs.k8s.io/secrets-store-sync-controller/pkg/k8s"
 	"sigs.k8s.io/secrets-store-sync-controller/pkg/provider"
 )
+
+type testCondition struct {
+	Type   string
+	Status corev1.ConditionStatus
+	Reason string
+}
+
+type testSecretSyncReconciler struct {
+	fakeProviderServer   *providerfake.MockCSIProviderServer
+	secretSyncReconciler *SecretSyncReconciler
+}
 
 func TestReconcile(t *testing.T) {
 	tests := []struct {
@@ -46,7 +59,8 @@ func TestReconcile(t *testing.T) {
 		secretProviderClassToProcess *secretsstorecsiv1.SecretProviderClass
 		secretSyncToProcess          *secretsyncv1alpha1.SecretSync
 		secret                       *corev1.Secret
-		err                          error
+		expectedErrorString          string
+		expectedCondition            *testCondition
 	}{
 		{
 			name: "creates secret successfully",
@@ -90,7 +104,325 @@ func TestReconcile(t *testing.T) {
 					"foo": []byte("bar"),
 				},
 			},
-			err: nil,
+			expectedCondition: &testCondition{
+				Type:   "Create",
+				Status: corev1.ConditionTrue,
+				Reason: "CreateSucceeded",
+			},
+		},
+		{
+			name: "SecretSync not found",
+			secretProviderClassToProcess: &secretsstorecsiv1.SecretProviderClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-spc",
+					Namespace: "default",
+				},
+				Spec: secretsstorecsiv1.SecretProviderClassSpec{
+					Provider: "fake-provider",
+					Parameters: map[string]string{
+						"foo": "v1",
+					},
+				},
+			},
+			secretSyncToProcess: &secretsyncv1alpha1.SecretSync{},
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"foo": []byte("bar"),
+				},
+			},
+			expectedErrorString: `secretsyncs.secret-sync.x-k8s.io "sse2esecret" not found`,
+		},
+		{
+			name: "invalid SecretObject returns validation error",
+			secretProviderClassToProcess: &secretsstorecsiv1.SecretProviderClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-spc",
+					Namespace: "default",
+				},
+				Spec: secretsstorecsiv1.SecretProviderClassSpec{
+					Provider: "fake-provider",
+					Parameters: map[string]string{
+						"foo": "v1",
+					},
+				},
+			},
+			secretSyncToProcess: &secretsyncv1alpha1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Spec: secretsyncv1alpha1.SecretSyncSpec{
+					ServiceAccountName:      "default",
+					SecretProviderClassName: "test-spc",
+					SecretObject: secretsyncv1alpha1.SecretObject{
+						Data: []secretsyncv1alpha1.SecretObjectData{
+							{
+								SourcePath: "foo",
+								TargetKey:  "bar",
+							},
+						},
+					},
+				},
+			},
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"foo": []byte("bar"),
+				},
+			},
+			expectedErrorString: "secret type is empty",
+			expectedCondition: &testCondition{
+				Type:   "Create",
+				Status: corev1.ConditionFalse,
+				Reason: "UserInputValidationFailed",
+			},
+		},
+		{
+			name: "use of reserved label returns validation error",
+			secretProviderClassToProcess: &secretsstorecsiv1.SecretProviderClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-spc",
+					Namespace: "default",
+				},
+				Spec: secretsstorecsiv1.SecretProviderClassSpec{
+					Provider: "fake-provider",
+					Parameters: map[string]string{
+						"foo": "v1",
+					},
+				},
+			},
+			secretSyncToProcess: &secretsyncv1alpha1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Spec: secretsyncv1alpha1.SecretSyncSpec{
+					ServiceAccountName:      "default",
+					SecretProviderClassName: "test-spc",
+					SecretObject: secretsyncv1alpha1.SecretObject{
+						Type: "Opaque",
+						Data: []secretsyncv1alpha1.SecretObjectData{
+							{
+								SourcePath: "foo",
+								TargetKey:  "bar",
+							},
+						},
+						Labels: map[string]string{
+							"secrets-store.sync.x-k8s.io": "test",
+						},
+					},
+				},
+			},
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"foo": []byte("bar"),
+				},
+			},
+			expectedErrorString: "label secrets-store.sync.x-k8s.io is reserved for use by the Secrets Store Sync Controller",
+			expectedCondition: &testCondition{
+				Type:   "Create",
+				Status: corev1.ConditionFalse,
+				Reason: "InvalidClusterSecretLabelError",
+			},
+		},
+		{
+			name: "use of reserved annotation returns validation error",
+			secretProviderClassToProcess: &secretsstorecsiv1.SecretProviderClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-spc",
+					Namespace: "default",
+				},
+				Spec: secretsstorecsiv1.SecretProviderClassSpec{
+					Provider: "fake-provider",
+					Parameters: map[string]string{
+						"foo": "v1",
+					},
+				},
+			},
+			secretSyncToProcess: &secretsyncv1alpha1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Spec: secretsyncv1alpha1.SecretSyncSpec{
+					ServiceAccountName:      "default",
+					SecretProviderClassName: "test-spc",
+					SecretObject: secretsyncv1alpha1.SecretObject{
+						Type: "Opaque",
+						Data: []secretsyncv1alpha1.SecretObjectData{
+							{
+								SourcePath: "foo",
+								TargetKey:  "bar",
+							},
+						},
+						Annotations: map[string]string{
+							"secrets-store.sync.x-k8s.io": "test",
+						},
+					},
+				},
+			},
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"foo": []byte("bar"),
+				},
+			},
+			expectedErrorString: "annotation secrets-store.sync.x-k8s.io is reserved for use by the Secrets Store Sync Controller",
+			expectedCondition: &testCondition{
+				Type:   "Create",
+				Status: corev1.ConditionFalse,
+				Reason: "InvalidClusterSecretAnnotationError",
+			},
+		},
+		{
+			name:                         "SecretProviderClass not found",
+			secretProviderClassToProcess: &secretsstorecsiv1.SecretProviderClass{},
+			secretSyncToProcess: &secretsyncv1alpha1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Spec: secretsyncv1alpha1.SecretSyncSpec{
+					ServiceAccountName:      "default",
+					SecretProviderClassName: "test-spc",
+					SecretObject: secretsyncv1alpha1.SecretObject{
+						Type: "Opaque",
+						Data: []secretsyncv1alpha1.SecretObjectData{
+							{
+								SourcePath: "foo",
+								TargetKey:  "bar",
+							},
+						},
+					},
+				},
+			},
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"foo": []byte("bar"),
+				},
+			},
+			expectedErrorString: `secretproviderclasses.secrets-store.csi.x-k8s.io "test-spc" not found`,
+			expectedCondition: &testCondition{
+				Type:   "Create",
+				Status: corev1.ConditionFalse,
+				Reason: "ControllerSPCError",
+			},
+		},
+		{
+			name: "failed to get provider client",
+			secretProviderClassToProcess: &secretsstorecsiv1.SecretProviderClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-spc",
+					Namespace: "default",
+				},
+				Spec: secretsstorecsiv1.SecretProviderClassSpec{
+					Provider: "invalid-fake-provider",
+					Parameters: map[string]string{
+						"foo": "v1",
+					},
+				},
+			},
+			secretSyncToProcess: &secretsyncv1alpha1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Spec: secretsyncv1alpha1.SecretSyncSpec{
+					ServiceAccountName:      "default",
+					SecretProviderClassName: "test-spc",
+					SecretObject: secretsyncv1alpha1.SecretObject{
+						Type: "Opaque",
+						Data: []secretsyncv1alpha1.SecretObjectData{
+							{
+								SourcePath: "foo",
+								TargetKey:  "bar",
+							},
+						},
+					},
+				},
+			},
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"foo": []byte("bar"),
+				},
+			},
+			expectedErrorString: `provider not found: provider "invalid-fake-provider"`,
+			expectedCondition: &testCondition{
+				Type:   "Create",
+				Status: corev1.ConditionFalse,
+				Reason: "ControllerSPCError",
+			},
+		},
+		{
+			name: "invalid SecretObjectData returns validation error",
+			secretProviderClassToProcess: &secretsstorecsiv1.SecretProviderClass{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-spc",
+					Namespace: "default",
+				},
+				Spec: secretsstorecsiv1.SecretProviderClassSpec{
+					Provider: "fake-provider",
+					Parameters: map[string]string{
+						"foo": "v1",
+					},
+				},
+			},
+			secretSyncToProcess: &secretsyncv1alpha1.SecretSync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Spec: secretsyncv1alpha1.SecretSyncSpec{
+					ServiceAccountName:      "default",
+					SecretProviderClassName: "test-spc",
+					SecretObject: secretsyncv1alpha1.SecretObject{
+						Type: "Opaque",
+						Data: []secretsyncv1alpha1.SecretObjectData{
+							{
+								SourcePath: "foo",
+								TargetKey:  "",
+							},
+						},
+					},
+				},
+			},
+			secret: &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "sse2esecret",
+					Namespace: "default",
+				},
+				Data: map[string][]byte{
+					"foo": []byte("bar"),
+				},
+			},
+			expectedErrorString: "target key in secretObject.data is empty",
+			expectedCondition: &testCondition{
+				Type:   "Create",
+				Status: corev1.ConditionFalse,
+				Reason: "UserInputValidationFailed",
+			},
 		},
 	}
 
@@ -98,7 +430,7 @@ func TestReconcile(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			ssc := newSecretSyncReconciler(t, scheme, test.secretProviderClassToProcess, test.secretSyncToProcess, test.secret)
+			testSecretSyncReconciler := newSecretSyncReconciler(t, scheme, test.secretProviderClassToProcess, test.secretSyncToProcess, test.secret)
 
 			// Mock request to simulate Reconcile being called
 			req := ctrl.Request{
@@ -108,12 +440,147 @@ func TestReconcile(t *testing.T) {
 				},
 			}
 
-			_, err := ssc.Reconcile(context.Background(), req)
-			if !errors.Is(err, test.err) {
+			_, err := testSecretSyncReconciler.secretSyncReconciler.Reconcile(context.Background(), req)
+			if len(test.expectedErrorString) > 0 {
+				if err == nil || err.Error() != test.expectedErrorString {
+					t.Fatalf("expected error %q, got %q", test.expectedErrorString, err)
+				}
+			} else if err != nil {
 				t.Fatalf("unexpected error: %v", err)
+			}
+
+			// validate status condition
+			condition := getSecretSyncStatusCondition(t, testSecretSyncReconciler.secretSyncReconciler, req)
+			if !reflect.DeepEqual(condition, test.expectedCondition) {
+				t.Fatalf("expected condition %v, got %v", test.expectedCondition, condition)
 			}
 		})
 	}
+}
+
+func TestConditionsOnHashChange(t *testing.T) {
+	secretProviderClassToProcess := &secretsstorecsiv1.SecretProviderClass{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-spc",
+			Namespace: "default",
+		},
+		Spec: secretsstorecsiv1.SecretProviderClassSpec{
+			Provider: "fake-provider",
+			Parameters: map[string]string{
+				"foo": "v1",
+			},
+		},
+	}
+	secretSyncToProcess := &secretsyncv1alpha1.SecretSync{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "sse2esecret",
+			Namespace: "default",
+		},
+		Spec: secretsyncv1alpha1.SecretSyncSpec{
+			ServiceAccountName:      "default",
+			SecretProviderClassName: "test-spc",
+			SecretObject: secretsyncv1alpha1.SecretObject{
+				Type: "Opaque",
+				Data: []secretsyncv1alpha1.SecretObjectData{
+					{
+						SourcePath: "foo",
+						TargetKey:  "bar",
+					},
+				},
+			},
+		},
+	}
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "sse2esecret",
+			Namespace: "default",
+		},
+		Data: map[string][]byte{
+			"foo": []byte("bar"),
+		},
+	}
+
+	scheme := setupScheme(t)
+	testSecretSyncReconciler := newSecretSyncReconciler(t, scheme, secretProviderClassToProcess, secretSyncToProcess, secret)
+
+	// Mock request to simulate Reconcile being called
+	req := ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      "sse2esecret",
+			Namespace: "default",
+		},
+	}
+
+	_, err := testSecretSyncReconciler.secretSyncReconciler.Reconcile(context.Background(), req)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	// simulate update with no secret value change
+	_, err = testSecretSyncReconciler.secretSyncReconciler.Reconcile(context.Background(), req)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	expectedCondition := &testCondition{
+		Type:   "Update",
+		Status: corev1.ConditionTrue,
+		Reason: "UpdateNoValueChangeSucceeded",
+	}
+	condition := getSecretSyncStatusCondition(t, testSecretSyncReconciler.secretSyncReconciler, req)
+	if !reflect.DeepEqual(condition, expectedCondition) {
+		t.Fatalf("expected condition %v, got %v", expectedCondition, condition)
+	}
+
+	// simulate update with secret value change
+	testSecretSyncReconciler.fakeProviderServer.SetFiles([]*v1alpha1.File{
+		{
+			Path:     "foo",
+			Mode:     0644,
+			Contents: []byte("bar"),
+		},
+	})
+	_, err = testSecretSyncReconciler.secretSyncReconciler.Reconcile(context.Background(), req)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	expectedConditionAfterSecretChange := &testCondition{
+		Type:   "Update",
+		Status: corev1.ConditionTrue,
+		Reason: "UpdateValueChangeOrForceUpdateSucceeded",
+	}
+	conditionAfterSecretChange := getSecretSyncStatusCondition(t, testSecretSyncReconciler.secretSyncReconciler, req)
+	if !reflect.DeepEqual(conditionAfterSecretChange, expectedConditionAfterSecretChange) {
+		t.Fatalf("expected condition %v, got %v", expectedConditionAfterSecretChange, conditionAfterSecretChange)
+	}
+}
+
+func getSecretSyncStatusCondition(t *testing.T, ssc *SecretSyncReconciler, req ctrl.Request) *testCondition {
+	t.Helper()
+
+	secretSync := getSecretSyncObject(t, ssc, req)
+	if len(secretSync.Status.Conditions) == 0 {
+		return nil
+	}
+
+	latestCondition := secretSync.Status.Conditions[len(secretSync.Status.Conditions)-1]
+
+	return &testCondition{
+		Type:   latestCondition.Type,
+		Status: corev1.ConditionStatus(latestCondition.Status),
+		Reason: latestCondition.Reason,
+	}
+}
+
+func getSecretSyncObject(t *testing.T, ssc *SecretSyncReconciler, req ctrl.Request) *secretsyncv1alpha1.SecretSync {
+	t.Helper()
+
+	secretSync := &secretsyncv1alpha1.SecretSync{}
+	err := ssc.Get(context.Background(), req.NamespacedName, secretSync)
+	if err != nil && !apierrors.IsNotFound(err) {
+		t.Fatalf("error getting secret sync: %v", err)
+	}
+
+	return secretSync
 }
 
 func newSecretSyncReconciler(
@@ -122,7 +589,7 @@ func newSecretSyncReconciler(
 	spc *secretsstorecsiv1.SecretProviderClass,
 	secretSync *secretsyncv1alpha1.SecretSync,
 	testSecret *corev1.Secret,
-) *SecretSyncReconciler {
+) *testSecretSyncReconciler {
 	t.Helper()
 
 	initObjects := []client.Object{
@@ -176,10 +643,14 @@ func newSecretSyncReconciler(
 		ProviderClients: providerClients,
 	}
 
-	return ssc
+	return &testSecretSyncReconciler{
+		fakeProviderServer:   server,
+		secretSyncReconciler: ssc,
+	}
 }
 
 func setupScheme(t *testing.T) *runtime.Scheme {
+	t.Helper()
 	scheme := runtime.NewScheme()
 
 	if err := secretsstorecsiv1.AddToScheme(scheme); err != nil {
